9a62936189c2753533f1701377974f695963eb2e
Boris Mikhaylov <boris.mikhaylov@getapp.com>
2015-01-14 09:24:08 +0100

    install plugins only when there is only Vundle present and run YouCompleteMe install.sh when compiled libs are missing

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+VIM_PLUGINS_PATH="$HOME/.vim/bundle"
+YOU_COMPLETE_ME_PATH="$VIM_PLUGINS_PATH/YouCompleteMe"
+
+# update submodules, specifically Vundle
 git submodule update --init --recursive
-vim +PluginInstall +qall
+
+# install vim plugins when there are none
+vim_plugins_count=$(find $VIM_PLUGINS_PATH/* -maxdepth 0 -type d | wc -l)
+if [[ $vim_plugins_count -eq 1 ]]; then
+  echo "installing vim plugins"
+  vim +PluginInstall +qall 
+fi
+
+# run install.sh of YouCompleteMe if it wasn't run before and Plugin is present
+if [ -d "$YOU_COMPLETE_ME_PATH" ] && [ ! -f "$YOU_COMPLETE_ME_PATH/third_party/ycmd/ycm_client_support.so" ]; then
+  echo 'youcomplete me found but not setup'
+  exec "$YOU_COMPLETE_ME_PATH/install.sh"
+fi
